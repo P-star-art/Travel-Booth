d23b6314318506ea566d57c4001d013a8b17bca6
Pranav <prn2108@gmail.com>
2021-05-02 17:41:48 +0530

    feat: adding UsersList component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserItem = (props) =>{
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+
+  const USERS = [
+    {
+      id: '1',
+      name: 'Pranav',
+      image: 'https://expertphotography.com/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg',
+      places: 3
+    },
+    {
+      id: '2',
+      name: 'Lahu',
+      image: 'https://expertphotography.com/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg',
+      places: 5
+    }
+  ]
+
+  return (
+    <UsersList items={USERS} />
+  )
 };
 
 export default Users;
