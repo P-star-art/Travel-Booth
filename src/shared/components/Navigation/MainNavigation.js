c918db62bb8bade4cf08a354003e1889d027f408
Pranav <prn2108@gmail.com>
2021-05-03 18:52:00 +0530

    feat: added backdrop for sideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = () => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
         <React.Fragment>
-            <SideDrawer>
+            {
+                drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />
+            }
+            <SideDrawer onClick={closeDrawerHandler} show={drawerIsOpen}>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
+
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button onClick={openDrawerHandler} className="main-navigation__menu-btn">
                     <span />
                     <span />
                     <span />
