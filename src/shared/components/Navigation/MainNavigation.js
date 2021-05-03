0a019fc255eb9ed4e9e247c2a157b7ac40d3c4cf
Pranav <prn2108@gmail.com>
2021-05-03 11:26:44 +0530

    feat: added navlinks to navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = () => {
@@ -16,7 +17,7 @@ const MainNavigation = () => {
                 <Link to="/">YourPlaces</Link>
             </h1>
             <nav>
-                ...
+                <NavLinks />
             </nav>
         </MainHeader>
     )
