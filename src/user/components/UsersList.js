574989090d9444ac5c276aff330eeb6a851e01d6
Pranav <prn2108@gmail.com>
2021-05-03 10:01:39 +0530

    feat: adding UserItem component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -11,7 +11,7 @@ const UsersList = (props) => {
     }
 
     return (
-        <ul>
+        <ul className="users-list">
             {
                 props.items.map(user => (
                     <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places} />
