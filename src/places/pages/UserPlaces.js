612c0cacd31f04a5813aec298f3d860f803171c7
Pranav <prn2108@gmail.com>
2021-05-04 07:51:48 +0530

    feat: added map feature

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -18,13 +18,13 @@ const DUMMY_PLACES = [
     },
     {
         id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the tallest building in New York',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/375px-Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St, New York, NY 10001, United States',
+        title: 'Taj Mahal',
+        description: 'One of the seven wonders of the world',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5-nNmWzILiz2HtwoKYcxoJL-CW6fqFOYa8Q&usqp=CAU',
+        address: 'Agra, Uttar Pradesh 282001, India.',
         location: {
-            lat: 40.7484405,
-            lng: -73.9878531
+            lat: 27.1750151,
+            lng: 78.0421552
         },
         creator: 'u2'
     }
