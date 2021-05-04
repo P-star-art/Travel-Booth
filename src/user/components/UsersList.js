eaeeeba7ef85dd4843358319a2640328e0da35d0
Pranav <prn2108@gmail.com>
2021-05-04 07:12:46 +0530

    feat: added modal for rendering map

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -15,7 +15,12 @@ const UsersList = (props) => {
         <ul className="users-list">
             {
                 props.items.map(user => (
-                    <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places} />
+                    <UserItem
+                        key={user.id}
+                        id={user.id}
+                        image={user.image}
+                        name={user.name}
+                        placeCount={user.places} />
                 ))
             }
         </ul>
