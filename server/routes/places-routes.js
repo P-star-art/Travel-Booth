ea9ade590c87184a761afda504ba7b536484703f
Pranav <prn2108@gmail.com>
2021-05-07 11:31:33 +0530

    feat: added user defined places and places routes along with error handling

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,10 +1,57 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('Request made');
-    res.json({"message" : "successufull"})
+const DUMMY_PLACE = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+          lat: 40.7484405,
+          lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p1',
+        title: 'Empire Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+          lat: 40.7484405,
+          lng: -73.9878584
+        },
+        creator: 'u2'
+      }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACE.find(p => {
+        return p.id === placeId;
+    })
+    if (!place) {
+        throw new HttpError('Could not find a place', 404);
+    }
+    res.json({ place });
+})
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const userPlace = DUMMY_PLACE.find(u => {
+        return u.creator === userId;
+    })
+    if (!userPlace) {
+        return next(new HttpError('Could not find a user place', 404));
+    }
+    res.json({ userPlace });
 })
 
 module.exports = router
